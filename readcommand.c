bba6fe7bb1b9f305edee7ed4f41640b4de49f750
maxnchief <logandogan@gmail.com>
2024-07-19 20:35:55 +0000

    betty complient

--- readcommand.c
+++ readcommand.c
@@ -7,13 +7,13 @@ readCommand - Function to take users input
  Return: return number of characters read from the starndard input
  */
 
- int readCommand(void)
- {
-    size_t n = 0;
-    char *buff = NULL;
-    int size;
+int readCommand(void)
+{
+   size_t n = 0;
+   char *buff = NULL;
+   int size;
 
-    size = getline(&buff, &n, stdin);
+   size = getline(&buff, &n, stdin);
    if (size == -1)
    {
       if (feof(stdin))
@@ -25,9 +25,9 @@ readCommand - Function to take users input
       {
       perror("Failed to read user input");
       free(buff);
-   }
+      }
    /*the code above accounts for input errors, and allocates in event of failure*/
     free(buff);
     return (size);
    }
- }
+}
