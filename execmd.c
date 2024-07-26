41c78591329639fe6e00d72ddebd45cead97ec05
maxnchief <logandogan@gmail.com>
2024-07-26 20:19:58 +0000

    added execute funtion

--- execmd.c
+++ execmd.c
@@ -1,4 +1,32 @@
 #include "shell.h"
 
 /**
-exeCmd - function to execute userr command */
+exeCmd - function to execute userr command 
+@argsC; an array of commands from the user
+Return: return 0 on success
+*/
+
+int exeCmd(char *argsC[])
+{
+    pid_t pid;
+    int exe;
+
+    pid = fork();
+
+    if (pid == -1)
+    {
+        perror("failed to create process");
+    }
+    else if (pid == 0)
+    {
+        exe = execve(argsC[0], argsC, NULL);
+        if (exe == -1)
+        {
+            perror("Error executing command");
+        }
+    }
+    else
+        wait();
+    
+    return (0);
+}
