22de91e8e2d5beec4a21ad71646adb1986358510
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2024-07-26 20:57:04 -0500

    Update Shell.c Added Parse

--- shell.c
+++ shell.c
@@ -16,3 +16,21 @@ Return: return 0 on sucsess
     }
     return (0);
 }
+
+/**
+ * parse_line - Split line into tokens (arguments)
+ * @line: Input line
+ *
+ * Return: Array of tokens
+ */
+char **parse_line(char *line)
+{
+        int bufsize = BUFFER_SIZE, position = 0;
+        char **tokens = malloc(bufsize * sizeof(char *));
+        char *token;
+
+        if (!tokens)
+        {
+                fprintf(stderr, "allocation error\n");
+                exit(EXIT_FAILURE);
+        }
