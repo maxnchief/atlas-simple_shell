7a46e2dbc19898eb9bbcfcbbe32c8ba8136deeed
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2024-07-28 13:24:09 -0500

    Update shell.c
    Accidently deleted whole thing, but fixed it

--- shell.c
+++ shell.c
@@ -1,1 +1,36 @@
+#include "shell.h"
 
+/**
+main - entry point
+Return: return 0 on sucsess
+ */
+
+ int main(void)
+
+ {
+    /*Loop to continue displaying a prompt */
+    while(1)
+    {
+        readCommand();
+
+    }
+    return (0);
+}
+
+/**
+ * parse_line - Split line into tokens (arguments)
+ * @line: Input line
+ *
+ * Return: Array of tokens
+ */
+char **parse_line(char *line)
+{
+        int bufsize = BUFFER_SIZE, position = 0;
+        char **tokens = malloc(bufsize * sizeof(char *));
+        char *token;
+
+        if (!tokens)
+        {
+                fprintf(stderr, "allocation error\n");
+                exit(EXIT_FAILURE);
+        }
