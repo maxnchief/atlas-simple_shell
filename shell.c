590f337501ccfcefd508c1a52a40a4c3a6c9317c
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2024-07-28 03:16:09 -0500

    Update shell.c
    Added execute

--- shell.c
+++ shell.c
@@ -34,3 +34,49 @@ char **parse_line(char *line)
                 fprintf(stderr, "allocation error\n");
                 exit(EXIT_FAILURE);
         }
+        
+/**
+ * execute - Execute shell built-in or program launch
+ * @args: Array of arguments
+ *
+ * Return: 1 if shell should stay on, 0 if dhell should terminate
+ */
+int execute(char **args)
+{
+	int builtin_status;
+	pid_t pid;
+	int status;
+
+	if (args[0] == NULL)
+	{
+		return (1);
+	}
+
+	builtin_status = handle_builtin_commands(args);
+	if (builtin_status != -1)
+	{
+		return (builtin_status);
+	}
+
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execvp(args[0], args) == -1)
+		{
+			perror("hsh")
+		}
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+	{
+		perror("hsh");
+	}
+	else
+	{
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+
+	return (1);
+}
